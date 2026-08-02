ff8996c6fbbac1ba5016b8dc97ac3ffe34fda1dc
SoumyadeepPal-art <soumyadeepp269@gmail.com>
2026-08-02 10:30:09 +0530

    Fix CORS configuration

--- backend/app/main.py
+++ backend/app/main.py
@@ -33,9 +33,10 @@ app = FastAPI(
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "https://edupilot-ai-wzlp.onrender.com",
-        "http://127.0.0.1:3000",
-    ],
+    "https://edupilot-ai-gtrf.onrender.com",  # Frontend
+    "http://127.0.0.1:3000",
+    "http://localhost:3000",
+     ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
